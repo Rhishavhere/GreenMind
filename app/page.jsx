bb5b52e8764d145aa2eb25abe9c55602d37accaa
Rhishavhere <rhishh.me@gmail.com>
2024-12-04 22:54:43 +0530

    update

--- app/page.jsx
+++ app/page.jsx
@@ -7,15 +7,16 @@ export default function Home() {
         <div className='hover:cursor-pointer'>ZoupAI</div>
         <div className='flex justify-center gap-10'>
           <p className='hover:cursor-pointer hover:scale-110 transition duration-300'>Home</p>
-          <p className='hover:cursor-pointer transition duration-300'>Services</p>
-          <p className='hover:cursor-pointer transition duration-300'>Contact</p>
+          <p className='hover:cursor-pointer hover:scale-110 transition duration-300'>Services</p>
+          <p className='hover:cursor-pointer hover:scale-110 transition duration-300'>Contact</p>
         </div>
         <div className='hover:cursor-pointer'>Sign In</div>
       </header>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        ENERGY SOLUTIONS WITH ZOUP AI
+      <div className="relative flex place-items-center">
+        <p className='font-bold text-6xl text-center'>
+          ENERGY SOLUTIONS <br/>WITH <span className='italic'>ZOUP AI</span></p>
       </div>
 
       </main>
